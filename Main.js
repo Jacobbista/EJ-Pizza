b32b304dd542b612700523b0b0cee7f69548a1c1
Jacobbista <43680452+Jacobbista@users.noreply.github.com>
2018-11-13 23:11:15 +0100

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,2 +1,16 @@
-#filejs
+function load(){
+  document.getElementById('filtri').addEventListener('click', barraFiltri, false)
 
+
+}
+function barraFiltri(){
+  var opzCont= document.getElementById('opzionicontainer');
+  var altCont= opzCont.style.height; //restituisce a console '' anche se dovrebbe essere '60px'
+  if (altCont == 60 + 'px') {
+    opzCont.style.height= 120 + 'px';
+  }
+  else {
+    opzCont.style.height= 60 + 'px';
+  }
+
+}
