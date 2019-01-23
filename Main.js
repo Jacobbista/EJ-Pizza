d3b663c287ad7e4abf64f008f6bd9f49379fbdf0
EmilianoFinetti <emilianofinetti@gmail.com>
2019-01-23 19:52:48 +0100

    Add files via upload

--- Main.js
+++ Main.js
@@ -2,15 +2,41 @@ function load(){
   document.getElementById('filtri').addEventListener('click', barraFiltri, false)
 
 
+
 }
 function barraFiltri(){
   var opzCont= document.getElementById('opzionicontainer');
   var altCont= opzCont.style.height; //restituisce a console '' anche se dovrebbe essere '60px'
   if (altCont == '60px') {
-    opzCont.style.height= '120px';
+    opzCont.style.height= '150px';
+    for (var i = 0; i <= 5; i++){
+      document.getElementsByClassName('check')[i].style.transitionDuration= (i+.3) + 's';
+      document.getElementsByClassName('labcheck')[i].style.transitionDuration= (i+.3) + 's';
+      document.getElementsByClassName('check')[i].style.opacity='1';
+      document.getElementsByClassName('labcheck')[i].style.opacity='1';
+    }
+    for (var i = 0; i <= 2; i++) {
+      document.getElementsByClassName('rad')[i].style.transitionDuration= (i+.5) + 's';
+      document.getElementsByClassName('labrad')[i].style.transitionDuration= (i+.5) + 's';
+      document.getElementsByClassName('rad')[i].style.opacity='1';
+      document.getElementsByClassName('labrad')[i].style.opacity='1';
+    }
   }
   else {
     opzCont.style.height= '60px';
+    for (var i = 5; i >= 0; i--) {
+      document.getElementsByClassName('check')[i].style.transitionDuration= (.5) + 's';
+      document.getElementsByClassName('labcheck')[i].style.transitionDuration= (.5) + 's';
+      document.getElementsByClassName('check')[i].style.opacity='0';
+      document.getElementsByClassName('labcheck')[i].style.opacity='0';
+    }
+    for (var i = 2; i >= 0; i--) {
+      document.getElementsByClassName('rad')[i].style.transitionDuration= (.5) + 's';
+      document.getElementsByClassName('labrad')[i].style.transitionDuration= (.5) + 's';
+      document.getElementsByClassName('rad')[i].style.opacity='0';
+      document.getElementsByClassName('labrad')[i].style.opacity='0';
+    }
+
   }
 
 }
---
+++ old/Main3.js
@@ -0,0 +1,38 @@
+function load(){
+  var bar1=document.getElementsByClassName('bar1')[0];
+  var bar2=document.getElementsByClassName('bar2')[0];
+  var barbox=document.getElementsByClassName('bar-box')[0];
+  var filtri=document.getElementById('filtri');
+  var filtriB=document.getElementById('filtriB');
+  filtri.addEventListener('click', function(){
+      if (filtri.style.width == '112.05px') {
+        filtri.style.width= '640px';
+        barbox.style.zIndex='1';
+        barbox.style.opacity='1';
+        filtriB.innerHTML='&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+        barbox.style.transform='translateX(-20px)';
+        bar1.style.transform='translateX(46px)rotate(45deg)';
+        bar2.style.transform='translateX(34px)rotate(-45deg)';
+        document.getElementsByClassName('f1')[0].style.display='flex';
+        document.getElementsByClassName('f1')[1].style.display='flex';
+        document.getElementsByClassName('f1')[2].style.display='flex';
+        document.getElementsByClassName('f1')[3].style.display='flex';
+      }
+      barbox.addEventListener('click', function(){
+        filtri.style.width= '112.05px';
+        barbox.style.opacity='0';
+        barbox.style.zIndex='0';
+        barbox.style.transform='translateX(20px)';
+        filtriB.innerHTML='Filtri';
+        barbox.style.transitionDelay='1s';
+        bar1.style.transform='translateX(-46px)rotate(-45deg)';
+        bar2.style.transform='translateX(-34px)rotate(45deg)';
+        barbox.style.transitionDelay='0s';
+        document.getElementsByClassName('f1')[0].style.display='none';
+        document.getElementsByClassName('f1')[1].style.display='none';
+        document.getElementsByClassName('f1')[2].style.display='none';
+        document.getElementsByClassName('f1')[3].style.display='none';
+
+      }, false);
+    }, false);
+  }
